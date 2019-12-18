0f3655289c6d3c15aeee2d7aa3459fe35de749d8
iPear <i.pear@outlook.com>
2019-12-18 14:53:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace BMPCompressor{
+
+    class Pixel{
+        int r;
+        int g;
+        int b;
+
+        Pixel(int r,int g,int b) : r(r),g(g),b(b){}
+
+    };
+
+    class Bitmap{
+        int width;
+        int height;
+        Pixel* data;
+
+        Bitmap(int width,int height):width(width),height(height),data(nullptr){}
+
+        void loadBMP(){
+
+        }
+
+    };
+
+
+    class HuffmanTreeNode{
+
+    };
+
+    class Huffman{
+
+    };
+
+}
 
 int main(){
-    std::cout<<"Hello, World!"<<std::endl;
-    return 0;
+
 }
