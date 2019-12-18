59a63d5fcf9a475e91ea4ade439f50cdad5063f3
iPear <i.pear@outlook.com>
2019-12-18 16:03:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-namespace BMPCompressor{
+template <typename T>
+class PriorityQueue{
 
-    class Pixel{
-        int r;
-        int g;
-        int b;
+public:
+    static const int INIT_CAPACITY=8;
+    int capacity;
+    int size;
+    T* data; // Index starts from 1
 
-        Pixel(int r,int g,int b) : r(r),g(g),b(b){}
+    PriorityQueue():size(0),capacity(INIT_CAPACITY){
+        data=new T[capacity];
+    }
 
-    };
+    void enlarge(){
+        capacity*=2;
+        data=(T*)realloc(data,capacity*sizeof(T));
+    }
 
-    class Bitmap{
-        int width;
-        int height;
-        Pixel* data;
+    void shiftUp(int i){
+        if(i==1)return;
+        while(i!=1){
+            if(data[i]<data[i/2]){
+                swap(data[i],data[i/2]);
+                i/=2;
+            }else{
+                return;
+            }
+        }
+    }
 
-        Bitmap(int width,int height):width(width),height(height),data(nullptr){}
+    void shiftDown(int i){
 
-        void loadBMP(){
+        int smallestIndex=i;
+
+        while(i*2<=size){
+
+            if(data[smallestIndex]>data[i*2]){ // If exist left son
+                smallestIndex=i*2;
+            }
+
+            if(i*2+1<=size){// If exist right son
+                if(data[smallestIndex]>data[i*2+1]){
+                    smallestIndex=i*2+1;
+                }
+            }
+
+            if(smallestIndex!=i){
+                swap(data[i],data[smallestIndex]);
+                i=smallestIndex;
+            }else{
+                return;
+            }
 
         }
 
-    };
+    }
 
+    void insert(const T& val){
+        if(size+1==capacity)enlarge();
+        data[size++]=val;
+        shiftUp(size-1);
+    }
+
+    const T& top()const{
+        return data[1];
+    }
+
+    void pop(){
+        assert(size>0);
+        data[1]=data[size--];
+        shiftDown(1);
+    }
+
+};
+
+namespace Compressor{
 
     class HuffmanTreeNode{
 
     };
 
-    class Huffman{
+    class HuffmanTree{
 
     };
 
